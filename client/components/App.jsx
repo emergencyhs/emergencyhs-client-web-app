8dfd0bb1e56d7be16db2f1120b79ccc625db0efe
Yassin Mziya <ymm22@cornell.edu>
2017-11-18 22:25:39 -0500

    tentative react enviornment set up

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+/*
+	./client/components/App.jsx
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class App extends React.Component {
+	render() {
+		return (
+			<div style={{textAlign: 'center'}}>
+        		<h1>Hello World</h1>
+      		</div>
+		);
+	}
+}
+
